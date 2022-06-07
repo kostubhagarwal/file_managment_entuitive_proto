4c5e05a1c1b433c2566cc8b2cfde960664c78f8e
kostubhagarwal <kostubh@outlook.com>
2022-06-07 15:12:18 -0400

    Scnd Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const q1 = document.querySelector('.q1')
+const q2 = document.querySelector('.q2')
+const q3 = document.querySelector('.q3')
+const q4 = document.querySelector('.q4')
+const outputs = document.querySelector('.outputs')
+const form = document.getElementById("frm1")
+
+// q2.classList.add('hidden')
+// q3.classList.add('hidden')
+// q4.classList.add('hidden')
+// outputs.classList.add('hidden')
+
+function myFunction () {
+    form.submit()
+}
+
+
+
