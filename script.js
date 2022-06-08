3ba1a74a50f9fdb991e0b8f2ad2af240c82ebecc
kostubhagarwal <kostubh@outlook.com>
2022-06-08 15:53:23 -0400

    MMVP

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ ca.classList.add('hidden')
 outputs.classList.add('hidden')
 naming.classList.add('hidden')
 
+let midfol = ["1 Admin", "2 Invest", "3 Design", "4 Draft", "5 CA"]
+let subfol0 = ["11 Promo", "12 PM", "13 EMAIL", "19 QM"]
+let subfol1 = ["21 Invest Info", "22 Photos", "23 Reports"]
+let subfol2 = ["31 BID Packages", "32 BIDs Received", "33 Design", "34 Model"]
+let subfol3 = ["41 ISSUED", "42 MARKUPS","43 STRUCT","44 ARCH","45 MECH","46 ELEC","47 LANDSCAPE","48 EXIST", "49 OTHER"]
+let subfol4 = ["51 Contract", "52 MM","53 SRR","54 Photos","55 SIs & SDNs","56 RFIs","57 Submittals","58 Shop Dwgs"]
+
 function myFunction () {
     q1.classList.add('hidden')
     q2.classList.remove('hidden')
@@ -32,7 +39,6 @@ function myFunction () {
 function myFunction2 () {
     q2.classList.add('hidden')
     q3.classList.remove('hidden')
-
 }
 
 function myFunction3 () {
@@ -42,35 +48,63 @@ function myFunction3 () {
 
     if ( x == 0 ) {
         admin.classList.remove('hidden')
+        sbfl = subfol0
     }   
     
     if (x == 1) {
         invest.classList.remove('hidden')
+        sbfl = subfol1
     }
     
     if (x == 2) {
         design.classList.remove('hidden')
+        sbfl = subfol2
     }
     
     if (x == 3) {
         draft.classList.remove('hidden')
+        sbfl = subfol3
     }
     
     if(x == 4) {
         ca.classList.remove('hidden')
+        sbfl = subfol4
     }
 }
 
 function myFunction4 () {
     q4.classList.add('hidden')
     naming.classList.remove('hidden')
+
+    if ( x == 0 ) {
+        y = document.getElementById("mySelect0").selectedIndex
+
+    }   
+    
+    if (x == 1) {
+        y = document.getElementById("mySelect1").selectedIndex
+
+    }
+    
+    if (x == 2) {
+        y = document.getElementById("mySelect2").selectedIndex
+
+    }
+    
+    if (x == 3) {
+        y = document.getElementById("mySelect3").selectedIndex
+
+    }
+    
+    if(x == 4) {
+        y = document.getElementById("mySelect4").selectedIndex
+    }
 }
 
 function myFunction5 () {
-    naming.classList.add('hidden')
+    // naming.classList.add('hidden')
     outputs.classList.remove('hidden')
-        // document.querySelector('.outputs').innerHTML = `P:\2022\${x}\${x}\${x}` 
-
+    document.querySelector('.outputs').innerHTML = `P:\\2022\\${document.getElementById("pnum").value} ${document.getElementById("pnme").value}\\${midfol[x]}\\${sbfl[y]}`
 }
 
 
